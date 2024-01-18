fda9a8219b5dce9e7d67c979919749feceecd332
Klervia Picaud <picaud.klervia@gmail.com>
2024-01-18 15:04:31 +0100

    Rien ne marche mais la deadline est là, tant pis

--- script.js
+++ script.js
@@ -1,3 +1,83 @@
+const sources = [
+    {
+        link: "https://www.linkedin.com/in/mohamad-almustafa-3502a469/",
+        img: "./assets/avatar/mohamad.webp"
+    },
+    {
+        link: "https://www.linkedin.com/in/caroline-berg%C3%A9-396646109/",
+        img: "./assets/avatar/caroline.webp"
+    },
+    {
+        link: "https://www.linkedin.com/in/elisa-bothy-a457b1225/",
+        img: "./assets/avatar/elisa.webp"
+    },
+    {
+        link: "https://www.linkedin.com/in/joan-paupardin-a448022a0/",
+        img: "./assets/avatar/joan.webp"
+    },
+    {
+        link: "https://www.linkedin.com/in/juliette-rat-b36b42216/",
+        img: "./assets/avatar/juliette.webp"
+    },
+    // {
+    //     link: "https://www.linkedin.com/in/valentina-sarais-85a2ab90/",
+    //     img: 
+    // },
+    // {
+    //     link: "https://www.linkedin.com/in/wiame-elalami/",
+    //     img: "https://www.linkedin.com/in/tha%C3%AFs-genin-291647271/",
+    // },
+    // {
+    //     link: "https://www.linkedin.com/in/k%C3%A9vin-hoppenot-47066b149/",
+    //     img: 
+    // },
+    // {
+    //     link: "https://www.linkedin.com/in/ralph-lee-a8b536213/",
+    //     img: 
+    // },
+    // {
+    //     link: "https://www.linkedin.com/in/igor-martellote/",
+    //     img: 
+    // },
+    // {
+    //     link: "https://www.linkedin.com/in/federico-manca-48a9981b9/",
+    //     img: 
+    // },
+    // {
+    //     link: "https://www.linkedin.com/in/klervia-picaud-9508022a0/",
+    //     img: 
+    // },
+    // {
+    //     link: "https://www.linkedin.com/in/guillaume-rostagnat-299002200/",
+    //     img: 
+    // },
+    // {
+    //     link: "https://www.linkedin.com/in/anastasiia-shlapak/",
+    //     img: 
+    // },
+    // {
+    //     link: "https://www.linkedin.com/in/am%C3%A9lie-solanas-pruvost-49a589242/",
+    //     img: 
+    // },
+    // {
+    //     link: "lien_pour_jour_17"
+    //     img: 
+    // }
+];
+
+  for (let i=1; i>5; i++)
+  {
+    const bouton=document.getElementById(i);
+    const link=document.createElement("a");
+    link.href=sources[i].link;
+    link.target="_blank";
+    const img=document.createElement("img");
+    img.src=sources[i].link;
+    img.alt="Image "+i;
+    img.class="day-image";
+    bouton.append(link, img);
+  }
+
 document.addEventListener("DOMContentLoaded", function () {
     const buttons = document.querySelectorAll(".day");
 
